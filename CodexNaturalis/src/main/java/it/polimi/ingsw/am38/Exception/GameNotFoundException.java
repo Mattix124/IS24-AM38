925cbcd04e500e5ffe6806a829cfbaf11e21c871
Davide-Grisanti <davide.grisanti@mail.polimi.it>
2024-04-18 18:37:45 +0200

    progressed with Controller classes and added a couple of Exceptions

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/am38/Exception/GameNotFoundException.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.am38.Exception;
+
+public class GameNotFoundException extends Exception{
+    public GameNotFoundException(String message){
+        super(message);
+    }
+}
