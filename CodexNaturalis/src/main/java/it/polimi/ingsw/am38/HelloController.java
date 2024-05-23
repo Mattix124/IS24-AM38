90d1a5a50e0a897c083897dc54bb436d8b9cdd9a
Francesco Giuseppe Spelta <francescogiuseppe.spelta@mail.polimi.it>
2024-05-23 17:12:22 +0200

    Login page layout fully working

--- CodexNaturalis/src/main/java/it/polimi/ingsw/am38/HelloController.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/am38/HelloController.java
@@ -1,14 +1,62 @@
 package it.polimi.ingsw.am38;
 
-import javafx.fxml.FXML;
+import it.polimi.ingsw.am38.Exception.NicknameTakenException;
+import it.polimi.ingsw.am38.Exception.NullNicknameException;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import it.polimi.ingsw.am38.Controller.LobbyManager;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+    public TextField textField;
+    public Button createButton;
+    public Button joinButton;
+    public Button okButton;
+    public Label promptLabel;
+    public Button backButton;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void joinButtonClicked() {
+        System.out.println("Join button clicked by " + textField.getText());
+        backButton.setVisible(true);
+
+        createButton.setVisible(false);
+        joinButton.setVisible(false);
+
+        okButton.setText("Join Game");
+        okButton.setVisible(true);
+
+        promptLabel.setText("Insert GameID");
+        textField.setPromptText("GameID");
+        // LobbyManager.getLobbyManager().createPlayer(textField.getText());
+    }
+
+    public void createButtonClicked() {
+        System.out.println("Create button clicked by " + textField.getText());
+        backButton.setVisible(true);
+
+        createButton.setVisible(false);
+        joinButton.setVisible(false);
+
+        okButton.setText("Create Game");
+        okButton.setVisible(true);
+
+        promptLabel.setText("Insert player number (2-4)");
+        textField.setPromptText("player number");
+        // LobbyManager.getLobbyManager().createPlayer(textField.getText());
+    }
+
+    public void backButtonClicked() {
+        System.out.println("Back button clicked by " + textField.getText());
+        backButton.setVisible(false);
+
+        createButton.setVisible(true);
+        joinButton.setVisible(true);
+
+        okButton.setText("HIDDEN");
+        okButton.setVisible(false);
+
+        promptLabel.setText("Nickname");
+        textField.setPromptText("Nickname");
     }
 }
